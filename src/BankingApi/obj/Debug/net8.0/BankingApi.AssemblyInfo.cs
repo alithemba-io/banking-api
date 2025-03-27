882ea5613c0d3089e656b053a05f996d8f057281
Alithemba Makongwana <a.makongwana@gmail.com>
2025-03-27 11:59:41 +0200

    Services

--- src/BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
+++ src/BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e33246d21ecb75f867041eeb061a8918c21b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08d5a2ae6f19714ac2ac8b6e03a4ffbde21e518")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
